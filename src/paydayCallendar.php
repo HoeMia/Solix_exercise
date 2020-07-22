666ddd5a6e442ce26d1119753396fbbcc18e016d
Paweł Staszkiewicz <thefunnysunny30@gmail.com>
2020-07-22 23:01:28 +0200

    Finished paydayCallendar functions and added tests

---
+++ src/paydayCallendar.php
@@ -0,0 +1,116 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use Carbon\Carbon;
+
+class PaydayCallendar
+{
+    private $date;
+    private $dateFormat;
+    private $noDayForThisMonthMessage;
+
+    public function __construct( $date_format = "d-m-Y", $formatted_date = "") 
+    {
+        $this->noDayForThisMonthMessage = "NONE";
+        $this->dateFormat = $date_format;
+        $formatted_date == "" ?
+            $this->date = Carbon::now()
+                :
+            $this->date = Carbon::createFromFormat( $this->dateFormat, $formatted_date );
+    }
+
+    public function getDateAsString() : string 
+    {
+        return $this->date->format( $this->dateFormat );
+    }
+
+    public function generatePaydayDatesForEachMonthTillEndOfYear()
+    {
+        $staticDate = clone $this->date;
+        $responseArray = [];
+        while( $staticDate->isSameYear( $this->date ) )
+        {
+            array_push( $responseArray, $this->getPaydaysForActualMonth() );
+            $this->goToNextMothFromActualDate(); 
+        }
+        return json_encode($responseArray);
+    }
+
+    public function getPaydaysForActualMonth()
+    {
+        $bonusPayday = $this->findBonusPaydayDateInActualMonth();
+        $rawPayday = $this->findRawPaydayDateInActualMonth();
+        return [ 'raw_payday' => $rawPayday, 'bonus_payday' => $bonusPayday ];
+    }
+
+    public function findRawPaydayDateInActualMonth() : string
+    {
+        $endOfMonthDate = $this->getLastWorkingDayDateOfTheGivenDate( clone $this->date );
+
+        if( $this->isActualDateGreaterThanGivenDate( $endOfMonthDate ) ) 
+        {
+            return $this->noDayForThisMonthMessage;
+        }
+
+        $endOfMonth = $endOfMonthDate->format( $this->dateFormat );
+        return $endOfMonth;
+    }
+
+    public function findBonusPaydayDateInActualMonth() : string
+    {
+        $bonusPaydayOfMonthDate = $this->getFifteenthDayOfMonth( clone $this->date );
+
+        if( $bonusPaydayOfMonthDate->isWeekend() ) 
+        {
+            $bonusPaydayOfMonthDate = $this->findNextWednesdayForGivenDate( clone $bonusPaydayOfMonthDate );
+        }
+
+        if( $this->isActualDateGreaterThanGivenDate( $bonusPaydayOfMonthDate ) ) 
+        {
+            return $this->noDayForThisMonthMessage;
+        }
+
+        $bonusPaydayOfMonth = $bonusPaydayOfMonthDate->format( $this->dateFormat );
+        return $bonusPaydayOfMonth;
+    }
+
+    private function isActualDateGreaterThanGivenDate( $date ): bool
+    {
+        return $this->date->greaterThan( $date );
+    }
+
+    private function getLastWorkingDayDateOfTheGivenDate( $date )
+    {
+        $endOfMonth = $date->endOfMonth()->format( $this->dateFormat ); 
+        $endOfMonthDate = Carbon::createFromFormat( $this->dateFormat, $endOfMonth );
+        while( $endOfMonthDate->isWeekend() )
+        {
+            $endOfMonthDate->subDay();
+        }
+        return $endOfMonthDate;
+    }
+
+    public function goToNextMothFromActualDate(): void
+    {
+        $this->date->startOfMonth();
+        $this->date->addMonth();
+    }
+
+    public function getNoDayForGivenMonthMessage(): string
+    {
+        return $this->noDayForThisMonthMessage;
+    }
+
+    private function getFifteenthDayOfMonth( $date )
+    {
+        $fifteenthOfMonth = $date->day(15);
+        return $fifteenthOfMonth;
+    }
+
+    private function findNextWednesdayForGivenDate( $date )
+    {
+        $date->next('Wednesday');
+        return $date;
+    }
+}
---
+++ tests/paydayCallendarTest.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use Carbon\Carbon;
+use PHPUnit\Framework\TestCase;
+
+final class PaydayCallendarTest extends TestCase
+{
+    protected static $callendar;
+    protected static $format;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$format = "d-m-Y";
+        self::$callendar = new PaydayCallendar( self::$format, "22-07-2020" );
+    }
+
+    public function testFindRawPaydayDateInActualMonth(): void
+    {
+        $actual = self::$callendar->findRawPaydayDateInActualMonth();
+        $expected = "31-07-2020";
+        $this->assertEquals( $actual, $expected );
+    }
+
+    public function testFindBonusPaydayDateInActualMonth(): void
+    {
+        $actual = self::$callendar->findBonusPaydayDateInActualMonth();
+        $expected = self::$callendar->getNoDayForGivenMonthMessage();
+        $this->assertEquals( $actual, $expected );
+    }
+
+    public function testCanGetActualDate(): void
+    {
+        $actual = self::$callendar->getDateAsString();
+        $expected = Carbon::now()->format( self::$format );
+        $this->assertEquals( $actual, $expected );
+    }
+
+    public function testGoToNextMonth(): void
+    {
+        self::$callendar->goToNextMothFromActualDate();
+        $actual = self::$callendar->getDateAsString();
+        $expected = "01-08-2020";
+        $this->assertEquals( $actual, $expected );
+    }
+
+    public function testGeneratePaydayDatesForEachMonthTillEndOfYear(): void
+    {
+        $actual = self::$callendar->generatePaydayDatesForEachMonthTillEndOfYear();
+        $expected = '[{"raw_payday":"31-08-2020","bonus_payday":"19-08-2020"},{"raw_payday":"30-09-2020","bonus_payday":"15-09-2020"},{"raw_payday":"30-10-2020","bonus_payday":"15-10-2020"},{"raw_payday":"30-11-2020","bonus_payday":"18-11-2020"},{"raw_payday":"31-12-2020","bonus_payday":"15-12-2020"}]';
+        $this->assertEquals( $actual, $expected );
+    }
+}
